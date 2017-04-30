fba661c5bdc4630c738d632f60a5ddb22d02ab11
Boshika Tara <boshikatara@gmail.com>
2017-04-29 20:42:31 -0700

    adds some initial iutp model analysis

--- pdb.py
+++ pdb.py
@@ -12,10 +12,44 @@ from __future__ import print_function
 from Bio import PDB
 from Bio.PDB import *
 
+
+####################### P53 1UTP MODEL ANALYSIS #######################
+
 #Using PDB to retrieve model from the list
-repository = PDB.PDBList()
-repository.retrieve_pdb_file('1TUP', pdir='.')
+# repository = PDB.PDBList()
+# repository.retrieve_pdb_file('1TUP', pdir='.')
 
 #Parse the file using mmcif, pdb parser is no longer supported by PDB
 parser = MMCIFParser()
-p53_1tup = parser.get_structure('P 53 - DNA Binding', '1tup.cif')
+p53_1tup = parser.get_structure('P 53 - DNA Binding', './protein_models/1tup.cif')
+
+#TOP DOWN ANALYSIS
+def describe_model(name, pdb):
+    print()
+    this_list = []
+    for model in pdb:
+        for chain in model:
+            this_list.append('%s - Chain: %s. Number of residues: %d. Number of atoms: %d.' % \
+                   (name, chain.id, len(chain), len(list(chain.get_atoms()))))
+    return this_list
+
+#chains in itup
+p53_itup_model = describe_model('1TUP', p53_1tup)
+print(p53_itup_model)
+
+#all residues except water
+residues = []
+for residue in p53_1tup.get_residues():
+    if residue.id[0] in [' ', 'H_HOH']:
+        continue
+    residues.append(residue.id)
+print(residues)
+
+#atom-related statistics
+
+
+
+
+# should be done using a better way, extracting data from uniprot API and hardcoding it defeats the purpose
+# pass this to proteomics datatbase table models
+# data = [(1, p53_itup_model)]
