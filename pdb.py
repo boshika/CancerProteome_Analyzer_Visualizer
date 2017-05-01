2b2efcee3546cee520edf0349d7c94aba89aae0b
Boshika Tara <boshikatara@gmail.com>
2017-04-30 18:59:36 -0700

    Sucess! created 3D scatter plots for first three proteins

--- pdb.py
+++ pdb.py
@@ -11,24 +11,17 @@ in, at the time of analyzes.
 from __future__ import print_function
 from Bio import PDB
 from Bio.PDB import *
-from pymol import cmd
-pymol.finish_launching()
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-
-
-
-
-####################### P53 1UTP MODEL ANALYSIS #######################
-
-#Using PDB to retrieve model from the list
-# repository = PDB.PDBList()
-# repository.retrieve_pdb_file('1TUP', pdir='.')
+repository = PDB.PDBList()
 
 #Parse the file using mmcif, pdb parser is no longer supported by PDB
 parser = MMCIFParser()
-p53_1tup = parser.get_structure('P 53 - DNA Binding', './protein_models/1tup.cif')
 
-#TOP DOWN ANALYSIS
+#######################   METHODS ######################
+
+#Number of Residues and atoms on Each Chain
 def describe_model(name, pdb):
     print()
     this_list = []
@@ -38,65 +31,126 @@ def describe_model(name, pdb):
                    (name, chain.id, len(chain), len(list(chain.get_atoms()))))
     return this_list
 
+#for generating 3d Plot
+def plot(model):
+    fig = plt.figure(figsize=(16, 9))
+    fig.suptitle(model, fontsize=20)
+    ax3d = fig.add_subplot(111, projection='3d')
+    ax_xy = fig.add_subplot(331)
+    ax_xy.set_title('X/Y')
+    ax_xz = fig.add_subplot(334)
+    ax_xz.set_title('X/Z')
+    ax_zy = fig.add_subplot(337)
+    ax_zy.set_title('Z/Y')
+    color = {'A': 'r', 'B': 'g', 'C': 'b', 'D': '0.25', 'E': '0.5', 'F': '0.75', 'G': '1', 'H': '1', 'J': '1'}
+    zx, zy, zz = [], [], []
+    for chain in model.get_chains():
+        xs, ys, zs = [], [], []
+        for residue in chain.get_residues():
+            ref_atom = next(residue.get_iterator())
+            x, y, z = ref_atom.coord
+            if ref_atom.element == 'HOH':
+                zx.append(x)
+                zy.append(y)
+                zz.append(z)
+                continue
+            xs.append(x)
+            ys.append(y)
+            zs.append(z)
+        ax3d.scatter(xs, ys, zs, color=color[chain.id])
+        print(color[chain.id])
+        ax_xy.scatter(xs, ys, marker='.', color=color[chain.id])
+
+        ax_xz.scatter(xs, zs, marker='.', color=color[chain.id])
+        ax_zy.scatter(zs, ys, marker='.', color=color[chain.id])
+    ax3d.set_xlabel('X')
+    ax3d.set_ylabel('Y')
+    ax3d.set_zlabel('Z')
+    ax3d.scatter(zx, zy, zz, color='k', marker='v', s=300)
+    ax_xy.scatter(zx, zy, color='k', marker='v', s=80)
+    ax_xz.scatter(zx, zz, color='k', marker='v', s=80)
+    ax_zy.scatter(zz, zy, color='k', marker='v', s=80)
+    for ax in [ax_xy, ax_xz, ax_zy]:
+        ax.get_yaxis().set_visible(False)
+        ax.get_xaxis().set_visible(False)
+
+    fig.savefig('./matplotlib_analysis/myc_1nkp.jpg')
+    # fig = plt.show()
+
+####################### P53 1UTP MODEL ANALYSIS #######################
+
+#Using PDB to retrieve model from the list
+# repository.retrieve_pdb_file('1TUP', pdir='.')
+
+p53_1tup = parser.get_structure('P 53 - DNA Binding', './protein_models/1tup.cif')
+
+#TOP DOWN ANALYSIS
+
 #chains in itup
 p53_itup_model = describe_model('1TUP', p53_1tup)
-print(p53_itup_model)
+# print(p53_itup_model)
 
-# #all residues except water
-# residues = []
-# for residue in p53_1tup.get_residues():
-#     if residue.id[0] in [' ', 'H_HOH']:
-#         continue
-#     residues.append(residue.id)
+# #all nonstandard residues except water
+residues = []
+for residue in p53_1tup.get_residues():
+    if residue.id[0] in [' ', 'H_HOH']:
+        continue
+    residues.append(residue.id)
 # print(residues)
-#
-# cmd.fetch(' 1TUP', async = False)
-# cmd.disable(' all')
-# cmd.enable(' 1TUP')
-# cmd.hide(' all')
-# cmd.show(' sphere', 'name zn')
-# cmd.scene(' S2', action =' store', view = 0, frame = 0, animate =-1)
-# cmd.set(' ray_trace_frames', 0)
-# cmd.mset( 1, 500)
-# cmd.frame(0)
-# cmd.scene('S0')
-# cmd.mview()
-# cmd.frame(60)
-# cmd.set_view((-0.175534308, -0.331560850, -0.926960170, 0.541812420, 0.753615797,
-#               -0.372158051, 0.821965039, -0.567564785, 0.047358301, 0.000000000,
-#               0.000000000, -249.619018555, 58.625568390, 15.602619171, 77.781631470,
-#               196.801528931, 302.436492920, -20.000000000))
-# cmd.mview()
-# cmd.frame(90)
-# cmd.set_view((-0.175534308, -0.331560850, -0.926960170, 0.541812420, 0.753615797,
-#               -0.372158051, 0.821965039, -0.567564785, 0.047358301, -0.000067875,
-#               0.000017881, -249.615447998, 54.029174805, 26.956727982, 77.124832153,
-#               196.801528931, 302.436492920, -20.000000000))
-# cmd.mview()
-# cmd.frame( 150)
-# cmd.set_view((-0.175534308, -0.331560850, -0.926960170, 0.541812420, 0.753615797, -0.372158051,
-#               0.821965039, -0.567564785, 0.047358301, -0.000067875, 0.000017881, -55.406421661,
-#               54.029174805, 26.956727982, 77.124832153, 2.592475891, 108.227416992, -20.000000000))
-# cmd.mview()
-#
-#
+
+#Pick a chain and look at its atoms
+res = next(p53_1tup[0]['A'].get_residues())
+# print(res)
+
+# plot(p53_1tup)
+
+####################### MYC 1NKP MODEL ANALYSIS #######################
+# repository.retrieve_pdb_file('1NKP', pdir='.')
+myc_1nkp = parser.get_structure('MYC - DNA Binding', './protein_models/1nkp.cif')
+
+#chains in 1nkp
+myc_1nkp_model = describe_model('1NKP', myc_1nkp)
+print(myc_1nkp_model)
+
+#all nonstandard residues except water
+count = 0
+for residue in myc_1nkp.get_residues():
+    if residue.id[0] in [' ', 'H_HOH']:
+        count +=1
+print("Most of the residues are HOH: {}".format(count))
+
+#Pick a chain and look at its atoms
+res1 = next(myc_1nkp[0]['F'].get_residues())
+print(res1)
+
+plot(myc_1nkp)
 
 
+####################### ERRB2 1N8Z MODEL ANALYSIS #######################
+# repository.retrieve_pdb_file('1N8Z', pdir='.')
+errb_1n8z = parser.get_structure('ERRB2-Complexed with Herceptin', './protein_models/1n8z.cif')
 
+#chains in 1nkp
+errb_1n8z_model = describe_model('1NKP', errb_1n8z)
+print(errb_1n8z_model)
 
+#all nonstandard residues except water
+residues1 = []
+for residue in errb_1n8z.get_residues():
+    if residue.id[0] in [' ', 'H_HOH']:
+        continue
+    residues1.append(residue.id)
+print(residues1)
 
+#Pick a chain and look at its atoms
+res1 = next(myc_1nkp[0]['A'].get_residues())
+print(res1)
 
+# plot(errb_1n8z)
 
-
-
-
-
-
-
-
-
-
-
-            # should be done using a better way, extracting data from uniprot API and hardcoding it defeats the purpose
+# should be done using a better way, extracting data from uniprot API and hardcoding it defeats the purpose
 # pass this to proteomics datatbase table models
-# data = [(1, p53_itup_model)]
+# data = [(1, 'P53 1UTP Model', p53_itup_model, residues, res),
+#         (2, 'MYC 1NKP Model', myc_1nkp_model, "Majority of residues are HOH, Total 962", res1
+#
+#         ]
